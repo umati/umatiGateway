ac5003d3b49df6ce341359e21d599a2c39f63e82
Sebastian Friedl <friedl.sebastian@web.de>
2025-02-10 09:16:21 +0100

    fix waitForContainer

--- Tests/integration/mqtt_test/start.sh
+++ Tests/integration/mqtt_test/start.sh
@@ -1,5 +1,8 @@
 export  IMAGE_REPOSITORY=umati/umatigateway
 export IMAGE_TAG=develop
-docker compose up
-#./waitForContainer.sh
-#python3 test_mqtt_sampleserver.py
+docker compose up -d
+python3 -m venv path/to/venv
+source path/to/venv/bin/activate
+pip3 install -r requirements.txt
+./waitForContainer.sh
+python3 test_mqtt_sampleserver.py
--- Tests/integration/mqtt_test/waitForContainer.sh
+++ Tests/integration/mqtt_test/waitForContainer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 NEXT_WAITTIME=0
 WAITTIME_LIMIT_SEC=600
-while [[ "$(docker logs  mqtt_test-gateway-1 | grep -c "nsu=http:_2F_2Fexample.com_2FFullMachineTool_2F")" != "2"  && "$NEXT_WAITTIME" != "$WAITTIME_LIMIT_SEC" ]]
+echo "Waiting for test container to become ready since ${NEXT_WAITTIME}s..."
+while [[ "$(docker logs  mqtt_test-gateway-1 | grep -c "Publish Bad List Maschine Nodes finish.")" -le "2"  && "$NEXT_WAITTIME" != "$WAITTIME_LIMIT_SEC" ]]
 do 
     echo "Waiting for test container to become ready since ${NEXT_WAITTIME}s..."
     sleep 5
