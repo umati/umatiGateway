fcc67db563aa67326e41d8fe8742cf16a1a5b8e8
Sebastian Friedl <friedl.sebastian@web.de>
2025-02-10 10:11:05 +0100

    improve ci workflow

--- Tests/integration/mqtt_test/test_mqtt_sampleserver.py
+++ Tests/integration/mqtt_test/test_mqtt_sampleserver.py
@@ -1,9 +1,6 @@
 """
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-Copyright 2023 (c) Sebastian Friedl, FVA GmbH / interop4X(for umati and VDW e.V.)
+SPDX-License-Identifier: Apache-2.0
+Copyright (c) 2025 Sebastian Friedl FVA GmbH - interop4x. All rights reserved.
 """
 
 import json
