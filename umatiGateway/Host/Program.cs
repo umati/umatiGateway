e5adb9d6a883e2a5f2b6a8029bd8f337bc5fc53e
Matthias Dornaus <matthias.dornaus@fva-service.de>
2025-06-10 16:43:47 +0200

    Implemented Ip Address configuration over WebUi Url. Allowed http connections.

--- umatiGateway/Host/Program.cs
+++ umatiGateway/Host/Program.cs
@@ -1,14 +1,31 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright (c) 2025 FVA GmbH - interop4x. All rights reserved.
+using NLog;
+using System;
+using System.Net;
 using umatiGateway.Core.OPC;
 using UmatiGateway;
 
+Logger Logger = LogManager.GetCurrentClassLogger();
 ClientFactory clientFactory = new ClientFactory();
 UmatiGatewayApp umatiGateway = clientFactory.getClient("Egal");
 if (umatiGateway.ActiveConfiguration.StartConfiguration.StartWebUI == true)
 {
     var builder = WebApplication.CreateBuilder(args);
-
+    string url = umatiGateway.ActiveConfiguration.WebUI.URL;
+    if(url != "")
+    {
+        if (Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
+            && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps)
+            && uriResult != null)
+        {
+                builder.WebHost.UseUrls(uriResult.ToString());
+        }
+        else
+        {
+            Logger.Error($"Invalid formatted Uri specified: {uriResult}");
+        }
+    }
     // Add services to the container
     builder.Services.AddRazorPages();
     builder.Services.AddControllers();
@@ -28,7 +45,7 @@ if (umatiGateway.ActiveConfiguration.StartConfiguration.StartWebUI == true)
         app.UseHsts();
     }
 
-    app.UseHttpsRedirection();
+    //app.UseHttpsRedirection();
     app.UseStaticFiles();
 
     app.UseRouting();
